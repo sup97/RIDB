dc9e60ab35a4e6d3d1b9e26ed7c8123ce1f36562
So Young Park <36643753+sup97@users.noreply.github.com>
2019-01-03 17:25:24 -0500

    .

--- combProcess2.R
+++ combProcess2.R
@@ -1,4 +1,4 @@
-combProcess <- function(data, N){  
+combProcess2 <- function(data, N){  
   occupancy <- ts(data$occupancy, frequency = 12, start = c(2007, 5))
   if(N<10){
     train <- window(occupancy, end=c(2017,8-N))
@@ -35,26 +35,9 @@ combProcess <- function(data, N){
     compare <- ts(occupancy[n:m], frequency = 12, start = c(2016,8)) 
   }
   
-  e <- abs((compare-fitted)/compare) * 100
-  e <- ifelse(is.na(e)==TRUE, 0, e)
-  e <- ifelse(e==Inf, 0, e)
-  
-  print(forecast::accuracy(Combination, occupancy))
-  print(mean(e))
-  
   gtemp.df = data.frame(Time=c(as.Date(time(compare))), 
                         gtemp=c(compare), 
-                        gtempl=c(Combination))
-  cbbPalette <- c("#D55E00", "#000000")
-  
-  ggplot(data = gtemp.df, aes(x=Time, y=value, color=series )  )             +
-    ylab('Occupancy (%)')                                 +
-    geom_line(aes(y=gtemp , col='Real'), size=1.2, alpha=.5)  +
-    geom_line(aes(y=gtempl, col='Combination'), size=1.2, alpha=.5)    +
-    theme(panel.background = element_blank()) + 
-    scale_y_continuous(breaks=seq(0,100,25)) +
-    scale_x_date(date_breaks = "2 month", date_labels = "%b %Y") +
-    scale_colour_manual(values=cbbPalette)
-
-  
+                        gtempl=c(Combination),
+                        gtempa=c(SARIMA$mean))
+  return(gtemp.df)
 }
--- mapeProcess.R
+++ mapeProcess.R
@@ -1,9 +1,27 @@
-combProcess <- function(data, N){  
+mapeProcess <- function(data, N){  
   occupancy <- ts(data$occupancy, frequency = 12, start = c(2007, 5))
   if(N<10){
     train <- window(occupancy, end=c(2017,8-N))
   } else {
     train <- window(occupancy, end=c(2016,7))
+  }
+  
+  n <- length(train)+1
+  m <- n+N-1
+  
+  if(N<10){
+    compare <- ts(occupancy[n:m], frequency = 12, start = c(2017,9-N))
+  } else {
+    compare <- ts(occupancy[n:m], frequency = 12, start = c(2016,8)) 
+  }
+ 
+  if (N>2) {MA <- forecast(ma(train, 3), h=N)
+  fitted <- MA$mean
+  
+  me <- abs((compare-fitted)/compare) * 100
+  me <- ifelse(is.na(me)==TRUE, 0, me)
+  me <- ifelse(me==Inf, 0, me)
+  print(mean(me))
   }
   
   SARIMA <- forecast(auto.arima(train, 
@@ -16,45 +34,41 @@ combProcess <- function(data, N){
                                 approximation= FALSE,
                                 allowmean = TRUE,
                                 allowdrift = TRUE), h=N)
+  fitted <- SARIMA$mean
+
+  se <- abs((compare-fitted)/compare) * 100
+  se <- ifelse(is.na(se)==TRUE, 0, se)
+  se <- ifelse(se==Inf, 0, se)
+  print(mean(se))
   
   ETS <- forecast(ets(train), h=N)
+  fitted <- ETS$mean
+  ee <- abs((compare-fitted)/compare) * 100
+  ee <- ifelse(is.na(ee)==TRUE, 0, ee)
+  ee <- ifelse(ee==Inf, 0, ee)
+  print(mean(ee))
+  
   NNAR <- forecast(nnetar(train, P=8, lambda = "auto", repeats = 100), h=N)
+  fitted <- NNAR$mean
+  ne <- abs((compare-fitted)/compare) * 100
+  ne <- ifelse(is.na(ne)==TRUE, 0, ne)
+  ne <- ifelse(ne==Inf, 0, ne)
+  print(mean(ne))
+  
   KNN <- knn_forecasting(train, h = N, lags = 1:12, k = 3, msas = "MIMO")
+  fitted <- KNN$prediction
+  ke <- abs((compare-fitted)/compare) * 100
+  ke <- ifelse(is.na(ke)==TRUE, 0, ke)
+  ke <- ifelse(ke==Inf, 0, ke)
+  print(mean(ke))
   
   Combination <- (ETS[["mean"]] + SARIMA[["mean"]] +
                     NNAR[["mean"]] + KNN[["prediction"]])/4
   
-  
   fitted <- Combination
-  n <- length(train)+1
-  m <- n+N-1
-  
-  if(N<10){
-    compare <- ts(occupancy[n:m], frequency = 12, start = c(2017,9-N))
-  } else {
-    compare <- ts(occupancy[n:m], frequency = 12, start = c(2016,8)) 
-  }
-  
+
   e <- abs((compare-fitted)/compare) * 100
   e <- ifelse(is.na(e)==TRUE, 0, e)
   e <- ifelse(e==Inf, 0, e)
-  
-  print(forecast::accuracy(Combination, occupancy))
   print(mean(e))
-  
-  gtemp.df = data.frame(Time=c(as.Date(time(compare))), 
-                        gtemp=c(compare), 
-                        gtempl=c(Combination))
-  cbbPalette <- c("#D55E00", "#000000")
-  
-  ggplot(data = gtemp.df, aes(x=Time, y=value, color=series )  )             +
-    ylab('Occupancy (%)')                                 +
-    geom_line(aes(y=gtemp , col='Real'), size=1.2, alpha=.5)  +
-    geom_line(aes(y=gtempl, col='Combination'), size=1.2, alpha=.5)    +
-    theme(panel.background = element_blank()) + 
-    scale_y_continuous(breaks=seq(0,100,25)) +
-    scale_x_date(date_breaks = "2 month", date_labels = "%b %Y") +
-    scale_colour_manual(values=cbbPalette)
-
-  
 }
